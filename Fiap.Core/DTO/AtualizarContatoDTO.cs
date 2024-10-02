472855f5eebb300eb158e044b5b5a14a9d54cc78
cleitonzummach <cleitonzummach@gmail.com>
2024-10-01 21:15:20 -0300

    Ajuste SH e AlterarContatoDTO

--- Fiap.Core/DTO/AtualizarContatoDTO.cs
+++ Fiap.Core/DTO/AtualizarContatoDTO.cs
@@ -2,7 +2,7 @@
 {
     public class AlterarContatoDTO
     {
-        public string Id { get; } = string.Empty;
+        public int Id { get; set; }
         public string? Nome { get; set; }
         public string? Ddd { get; set; }
         public string? Telefone { get; set; }
